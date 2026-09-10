53e138428200908c2db558c277730dae70382734
EnriqueLanda2 <20233tn174@utez.edu.mx>
2026-09-09 20:16:14 -0600

    Agrega funcionalidad adicional

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// Feature: misión secreta
+const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+let konamiIndex = 0;
+
+document.addEventListener('keydown', (e) => {
+    // Pendiente de implementar
+});
