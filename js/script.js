55e3cc44df4df789602ba3e3dd34d3d5150e4ef7
EnriqueLanda2 <20233tn174@utez.edu.mx>
2026-09-09 20:16:32 -0600

    Ajusta comportamiento de funcionalidad adicional

--- js/script.js
+++ js/script.js
@@ -4,5 +4,16 @@ const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft',
 let konamiIndex = 0;
 
 document.addEventListener('keydown', (e) => {
-    // Pendiente de implementar
+    if (e.key === konamiCode[konamiIndex]) {
+        konamiIndex++;
+        if (konamiIndex === konamiCode.length) {
+            alert('🕵️ ¡Misión Secreta completada! Has activado el Código Konami.');
+            document.body.style.transition = "all 1s";
+            document.body.style.backgroundColor = "#222";
+            document.body.style.color = "#0f0";
+            konamiIndex = 0;
+        }
+    } else {
+        konamiIndex = 0;
+    }
 });
